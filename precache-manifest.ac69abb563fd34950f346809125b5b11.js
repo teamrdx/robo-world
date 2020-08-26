096fb5dfbc86841c4cd6c1f07750000a042cd0e8
Biplav Dahal <bipzification@gmail.com>
2020-08-26 12:16:34 +0200

    Updates

---
+++ precache-manifest.ac69abb563fd34950f346809125b5b11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61c8fbcc8bbc0d85914840dfbe6cafb",
+    "url": "/robo-world/index.html"
+  },
+  {
+    "revision": "4ca3497d2b7014a169a3",
+    "url": "/robo-world/static/css/main.b7bbc53b.chunk.css"
+  },
+  {
+    "revision": "e8668558102289940d4f",
+    "url": "/robo-world/static/js/2.99f2b2fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-world/static/js/2.99f2b2fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca3497d2b7014a169a3",
+    "url": "/robo-world/static/js/main.45baa431.chunk.js"
+  },
+  {
+    "revision": "9f5e8ec119365f8e10bd",
+    "url": "/robo-world/static/js/runtime-main.729817a2.js"
+  }
+]);
